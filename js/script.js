200059bc87e886cc83f2c516d867b4d1703fa5b3
Yunus <seytnazarov.yunus@gmail.com>
2022-07-21 23:53:49 +0400

    Create data labels & datasets

--- js/script.js
+++ js/script.js
@@ -3,3 +3,31 @@
 import users from "./user_rating.js";
 console.log(users)
 
+const data = {
+  labels: users.map(item => {
+    const getFullName = () => {
+      return `${item.firstName} ${item.lastName}`
+    }
+    return getFullName();
+  }),
+  datasets: [
+    {
+      label: 'Successful indicators',
+      data: users.map(user => user.rating),
+      borderColor: 'red',
+      backgroundColor: 'hsl(125%, 100%, 50%, 0.5)',
+      borderWidth: 2,
+      borderRadius: Number.MAX_VALUE,
+      borderSkipped: false,
+    },
+    {
+      label: 'Current rating',
+      data: users.map(user => user.rating),
+      borderColor: 'blue',
+      backgroundColor: 'hsl(250, 100%, 50%, 0.5)',
+      borderWidth: 2,
+      borderRadius: 5,
+      borderSkipped: false,
+    }
+  ]
+};
